ab2bb69934623a9f6e4989217d62502591c3cafc
Becca Bailey <beccanelsonbailey@gmail.com>
2019-08-28 15:57:25 -0500

    Add exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { createGame } from "./game";
+export { createBoard } from "./utils";
+export { getBestMove } from "./ai";
+export { isFull } from "./board";
+export { getNextPlayer, getWinner, advanceGameState, isOver } from "./rules";
+export * from "./types";
